5fa793fef123c0d1c67e47b1e8176650257299a2
joekennerly <joseph.kennerly@gmail.com>
2019-10-03 14:22:17 -0500

    aded order

--- bangazon_api/bangazon/models/order.py
+++ bangazon_api/bangazon/models/order.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Order(models.Model):
 
-    customer_id = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
-    payment_id = models.ForeignKey(Payment, on_delete=models.DO_NOTHING)
-    order_placed_date = models.DateField(null=True, blank=True, default=)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    payment_id = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    order_placed_date = models.DateField(null=True, blank=True, default=None)
 
     class Meta:
         verbose_name = ("order")
