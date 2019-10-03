0d961b07491b8929b0c7ea31cf142948287a83c3
MelanieJaneBond <miss.bond2@gmail.com>
2019-10-03 15:57:34 -0500

    capitalization

--- bangazon_api/bangazon/models/product.py
+++ bangazon_api/bangazon/models/product.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     customer_id = models.ForeignKey("Customer", on_delete = models.CASCADE)
     price = models.DecimalField(max_digits = 100, decimal_places = 2)
     description = models.CharField(max_length = 300)
-    product_category_id = models.ForeignKey("Product_category", on_delete = models.CASCADE)
+    product_category_id = models.ForeignKey("Product_Category", on_delete = models.CASCADE)
     quantity_available = models.IntegerField()
     quantity_sold = models.IntegerField()
     date_created = models.DateField(null=True)
