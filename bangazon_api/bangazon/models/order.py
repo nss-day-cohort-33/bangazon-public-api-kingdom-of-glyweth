cf84756c0f70bddb529713124df7aa4122759008
joekennerly <joseph.kennerly@gmail.com>
2019-10-03 14:20:25 -0500

    added bangazon

---
+++ bangazon_api/bangazon/models/order.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Order(models.Model):
+
+    customer_id = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+    payment_id = models.ForeignKey(Payment, on_delete=models.DO_NOTHING)
+    order_placed_date = models.DateField(null=True, blank=True, default=)
+
+    class Meta:
+        verbose_name = ("order")
+        verbose_name_plural = ("orders")
