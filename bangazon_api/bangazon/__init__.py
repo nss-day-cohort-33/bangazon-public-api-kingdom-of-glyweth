390900ec1f8cb09970d931390f5b4a16b70afb9d
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-10-04 10:39:44 -0500

    migrations worksga .!

--- bangazon_api/bangazon/__init__.py
+++ bangazon_api/bangazon/__init__.py
@@ -1,1 +1,6 @@
-from .models import *
+# from .models import product
+# from .models import payment
+# from .models import customer
+# from .models import order
+# from .models import order_product
+# from .models import product_category
---
+++ bangazon_api/bangazon/models/__init__.py
@@ -0,0 +1,6 @@
+from .customer import Customer
+from .order_product import Order_Products
+from .order import Order
+from .payment import Payment
+from .product import Product
+from .product_category import Product_Category
