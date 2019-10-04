afa34037c19a3d56246b82a8f25e34b0a5ca228f
Sydney Noh <sydneycnoh@gmail.com>
2019-10-04 15:05:35 -0500

    Most of views is done, need to check it once customer view is merged

---
+++ bangazon_api/bangazon/views/payment.py
@@ -0,0 +1,114 @@
+"""View module for handling requests about Customer Payment Methods"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from bangazon.models import Payment, Customer
+
+
+class PaymentSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for Payment
+
+    Arguments:
+        serializers
+    """
+    class Meta:
+        model = Payment
+        url = serializers.HyperlinkedIdentityField(
+            view_name='payment',
+            lookup_field='id'
+        )
+        fields = ('id', 'url', 'merchant_name', 'account_number', 'customer', 'creation_date', 'expiration_date')
+
+
+class Payments(ViewSet):
+    """Customer Payment Methods for Bangazon"""
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized Payment instance
+        """
+        new_payment = Payment()
+        new_payment.merchant_name = request.data["merchant_name"]
+        new_payment.account_number = request.data["account_number"]
+        new_payment.creation_date = request.data["creation_date"]
+        new_payment.expiration_date = request.data["expiration_date"]
+
+        customer = Customer.objects.get(pk=request.data["customer_id"])
+        new_payment.customer = customer
+        new_payment.save()
+
+        serializer = PaymentSerializer(new_payment, context={'request': request})
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single customer payment method
+
+        Returns:
+            Response -- JSON serialized customer instance
+        """
+        try:
+            customer = Customer.objects.get(pk=pk)
+            serializer = PaymentSerializer(customer, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a customer payment method
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        payment = payment.objects.get(pk=pk)
+        customer = Customer.objects.get(pk=request.data["customer_id"])
+        payment.merchant_name = request.data["merchant_name"]
+        payment.account_number = request.data["account_number"]
+        payment.creation_date = request.data["creation_date"]
+        payment.expiration_date = request.data["expiration_date"]
+        payment.customer = customer
+        payment.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single customer payment method
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            this_payment = Payment.objects.get(pk=pk)
+            this_payment.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Payment.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def list(self, request):
+        """Handle GET requests to customer payment resource
+
+        Returns:
+            Response -- JSON serialized list of park attractions
+        """
+        # customer = Customer.objects.get(user=request.auth.user)
+        # payment = payment.objects.filter(customer=customer)
+        # serializer = PaymentSerializer(
+        #     payment, many=True, context={'request': request})
+        # return Response(serializer.data)
+
+        payment = Payment.objects.all()
+
+        serializer = PaymentSerializer(
+            payment, many=True, context={'request': request})
+        return Response(serializer.data)
+
+        
