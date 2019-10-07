f303f0462ccdfa7e42cdb1d24f3ae0b0049be4b5
joekennerly <joseph.kennerly@gmail.com>
2019-10-07 15:30:15 -0500

    it work

--- bangazon_api/bangazon_api/settings.py
+++ bangazon_api/bangazon_api/settings.py
@@ -48,7 +48,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
@@ -67,8 +67,8 @@ MIDDLEWARE = [
 
 # This is new
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080',
-    'http://127.0.0.1:8080'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
 )
 
 ROOT_URLCONF = 'bangazon_api.urls'
