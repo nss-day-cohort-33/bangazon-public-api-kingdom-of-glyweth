673e5807d3fd8c0f73e4afe9c888d249b21a90ae
joekennerly <joseph.kennerly@gmail.com>
2019-10-07 09:35:25 -0500

    cat

--- bangazon_api/bangazon/views/__init__.py
+++ bangazon_api/bangazon/views/__init__.py
@@ -3,3 +3,4 @@ from .order_product import  Order_Products_2
 from .register import login_user
 from .register import register_user
 from .customer import Customers
+from .product_category import Product_Categories
