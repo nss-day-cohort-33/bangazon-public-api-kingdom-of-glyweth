e2367e9ae30f194c740263243f959d38b3b71100
berkeley platte <berkeleyplatte989@gmail.com>
2019-10-09 10:34:55 -0500

    meh

--- bangazon_api/bangazon_api/settings.py
+++ bangazon_api/bangazon_api/settings.py
@@ -48,7 +48,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
@@ -70,6 +70,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://localhost:3000',
+    'http://127.0.0.1:8000',
 )
 
 ROOT_URLCONF = 'bangazon_api.urls'
