ffe1745d0b86231c8a5dd2591f0ac40af4e577c6
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-10-09 11:06:23 -0500

    updates to payment

--- bangazon_api/bangazon/models/payment.py
+++ bangazon_api/bangazon/models/payment.py
@@ -7,7 +7,7 @@ class Payment(models.Model):
     merchant_name = models.CharField(max_length=25)
     account_number = models.CharField(max_length=25)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    creation_date = models.DateField(null=True, blank=True, default=None)
+    creation_date =  models.DateTimeField(auto_now_add=True)
     expiration_date = models.DateField(null=True, blank=True, default=None)
 
     class Meta:
