dcb4583bac0c087b79bc50487f2ee69a40bc173f
joekennerly <joseph.kennerly@gmail.com>
2019-10-14 15:35:25 -0500

    order

--- bangazon_api/bangazon/models/order.py
+++ bangazon_api/bangazon/models/order.py
@@ -6,7 +6,7 @@ from .payment import Payment
 class Order(models.Model):
     """Model for order"""
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
-    payment = models.ForeignKey(Payment, on_delete=models.DO_NOTHING)
+    payment = models.ForeignKey(Payment, on_delete=models.DO_NOTHING, blank=True, null=True)
     order_placed_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
