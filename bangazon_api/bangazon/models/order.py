8e83fd528f3b2a6a9ebd22ac4f5b6b39fdc427fa
joekennerly <joseph.kennerly@gmail.com>
2019-10-15 08:57:17 -0500

    added comments to order; changed customer on_delete to cascade

--- bangazon_api/bangazon/models/order.py
+++ bangazon_api/bangazon/models/order.py
@@ -1,11 +1,18 @@
-"""Model for order"""
+"""
+Joe Kennerly
+Model for order
+"""
 from django.db import models
 from .customer import Customer
 from .payment import Payment
 
 class Order(models.Model):
-    """Model for order"""
-    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+    """Model for order
+    customer - An order is dependent on a customer. If a customer is removed from the db, remove associated orders.
+    payment - A payment is required to indicate a completed order. On creation, payment will be nothing.
+    order_placed_date - Save the current date value on creation of an order.
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     payment = models.ForeignKey(Payment, on_delete=models.DO_NOTHING, blank=True, null=True)
     order_placed_date = models.DateTimeField(auto_now_add=True)
 
